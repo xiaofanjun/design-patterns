caa4cbd3d3a3327f9a4fe667a8e54de528fe95cb
unknown <zhangq_xinxi@sinosure.com.cn>
2020-01-10 19:53:49 +0800

    添加线程相关问题

--- src/main/java/thread/ThreadBaseDemo.java
+++ src/main/java/thread/ThreadBaseDemo.java
@@ -5,7 +5,8 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * 线程基础相关
+ * -----------------------------------------------线程基础----------------------------------------------------------
+ * <p>
  * <p>
  * 1：生命周期: new(新建)-->start(就绪)-->run(运行)-->block(sleep|wait)（堵塞）-->destroy（销毁）。
  * <p>
@@ -13,13 +14,45 @@ import java.util.concurrent.locks.ReentrantLock;
  * <p>
  * 3：进程与线程区别：进程是指在操作系统中正在运行的一个应用程序；线程是指进程内独立执行某个任务的一个单元；一个进程中可以包含多个线程。
  * <p>
- * 4：synchronized和Lock的区别：{@link ThreadBase4}
+ * 4：synchronized和Lock的区别：
  * &nbsp;(1)：synchronized是Java内置的关键字，在jvm层面，Lock是Java接口;
- * &nbsp;(2)：synchronized无法判断是否获取到锁，Lock可以；
- * &nbsp;(3)：synchronized可以自动释放锁，Lock需要在finally中手动释放锁（容易造成死锁）；
- * &nbsp;(4)：synchronized当有线程等待时，会一直等，而Lock可以不用等，直接结束。
+ * &nbsp;(2)：synchronized无法判断是否获取到锁，Lock可以；{@link ThreadBase4}
+ * &nbsp;(3)：synchronized可以自动释放锁，Lock需要在finally中手动释放锁（容易造成死锁）；{@link ThreadBase4}
+ * &nbsp;(4)：synchronized当有线程等待时，会一直等，而Lock可以不用等，直接结束(使用tryLock())。{@link ThreadBase4}
  * &nbsp;(5): synchronized的锁可重入，不可中断，非公平，而Lock可重入，可中断，可公平。
  * &nbsp;(6): Lock锁适合大量同步的代码同步问题，synchronized适合少量的。
+ * <p>
+ * 5: sleep()和wait()的区别:
+ * <p>
+ * 6: 如何模拟一个死锁：
+ * <p>
+ * 7：深入理解ThreadLocal的实现原理：
+ * <p>
+ * 8：synchronized的偏向锁，轻量级锁，重量级锁的理解：
+ * <p>
+ * 9：分别说下 JVM 和 JDK 层面 synchronized是如何保证线程安全的：
+ * <p>
+ * 10：写一个线程安全的单例：
+ * <p>
+ * 11：线程池启动线程submit和execute的区别： submit 中还是调用的 execute,不同的是 submit可以有返回值。
+ * <p>
+ * 12：synchronized 修饰 静态方法和非静态方法有啥区别：{@link SyncTest2}{@link SyncTest3}
+ * <p>
+ * 13：当一个线程进入一个对象的一个 synchronized方法后，其他线程是否可进入到此对象的该方法:{@link SynchronizedDemo}
+ * &nbsp; (1)、如果该锁是对象中的方法锁（也叫this锁），那么如果其他线程是同一个对象，那么则不能进入，不是同一个对象则可以进入。
+ * &nbsp: (2)、如果该锁是static锁，那么其他线程都不能进入。
+ * <p>
+ * 14: SimpleDateFormat 是线程安全的吗，如何解决:
+ * <p>
+ * 15: ConcurrentHashMap 底层实现原理:
+ * <p>
+ * 16: 线程池的原理，为什么要创建线程池，创建线程池的方式：
+ * <p>
+ * 17：创建线程池的核心参数，如何合理设置线程池大小：
+ * <p>
+ * 18: 通过三种方式实现生产者消费者模式:
+ * <p>
+ * 19：乐观锁，悲观锁：
  */
 public class ThreadBaseDemo {
 }
@@ -119,15 +152,41 @@ class ThreadBase2_1 implements Runnable {
 /**
  * Lock 锁
  */
-class ThreadBase4 {
+class ThreadBase4 implements Runnable {
     private Lock lock = new ReentrantLock();
 
-    public void testLock() {
-
+    public void testLock(Thread thread) throws InterruptedException {
+        //获取锁
+        //lock.lock();
+        //尝试获取锁，如果不到只等3秒，如果3秒后还获取不到，返回false
+        if (lock.tryLock(3000, TimeUnit.MICROSECONDS)) {
+            try {
+                System.out.println("线程" + thread.getName() + "获取当前锁");
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println("线程" + thread.getName() + "发生异常并释放锁");
+            } finally {
+                System.out.println("线程" + thread.getName() + "执行完毕并释放锁");
+                lock.unlock();//释放锁
+            }
+        }
     }
 
     public static void main(String[] args) {
-
+        ThreadBase4 threadBase4 = new ThreadBase4();
+        Thread thread1 = new Thread(threadBase4);
+        Thread thread2 = new Thread(threadBase4);
+        thread1.start();
+        thread2.start();
     }
 
+    @Override
+    public void run() {
+        try {
+            testLock(Thread.currentThread());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
